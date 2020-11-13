7f0e63ca6ca170174bb7e240b4e88816e1cee4b1
check13 <h13ksouri@gmail.com>
2020-11-13 23:24:54 +0100

    add raten

---
+++ raten.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class raten {
+    public static void SelbstRaten() {
+        double numberToGuess = (int) (Math.random() * (100 - 1 + 1) + 1);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter the first number");
+        int nextNumber = scanner. nextInt () ;
+        while ( nextNumber != numberToGuess ) {
+            if (nextNumber < numberToGuess) {
+                System.out.println("The number must be bigger then  " + nextNumber);
+                nextNumber = scanner.nextInt() ;
+            } else if (nextNumber > numberToGuess){
+
+                System.out.println("The number must be under then " + nextNumber);
+                nextNumber = scanner.nextInt();
+            }
+
+        } System.out.println("congratulation you guessed the right number ");
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        if (args.length==0){
+            SelbstRaten();
+        }
+
+    }
+}
